038dc6d46334e64fd03ed4c42d484d1cffc1a2a3
Thomas.Bianconi <Thomas.Bianconi@FRIDFWKS103184.emea.loreal.intra>
2016-05-27 16:32:28 +0200

    Init GhitHub

---
+++ LOREAL Sand Box/src/com/loreal/sandbox/client/LOREAL_Sand_Box.java
@@ -0,0 +1,11 @@
+package com.loreal.sandbox.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class LOREAL_Sand_Box implements EntryPoint {
+	public void onModuleLoad() {
+	}
+}
